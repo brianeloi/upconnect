450592566af377c40a087f8ba5464254b2899ba3
Brian Eloi <briansanteloi@gmail.com>
2024-08-13 17:25:50 -0300

    site updated

--- js/initial_page/main_page_body.js
+++ js/initial_page/main_page_body.js
@@ -3,11 +3,6 @@ import { formContent } from '../../js/form_page/form_content.js'
 export const mainPageBody = () => {
 
     let body =  '<div class="main_page_body">' +
-                    /*'<div class="base_font_color main_page_links">' +
-                        '<a href="https://www.instagram.com/upconnectweb/" target="_blank">'+
-                            'Instagram'+
-                        '</a>' +
-                    '</div>' +*/
                     '<div class="first_page_left_container">'+
                         '<div class="main_page_title base_font_color">Transforme sua <br>Presença Digital</div>'+
                         '<div class="main_page_text base_font_color">' +
@@ -22,20 +17,209 @@ export const mainPageBody = () => {
                         '</div>'+
                     '</div>'+
                     '<div class="first_image_container">'+
-                        '<img class="first_image" src="images/cyber2077boy.png" alt="cyber boy">'
+                        '<img class="first_image" src="images/cyber2077boy.png" alt="cyber boy">'+
                     '</div>'+
                 '</div>'
 
-    let body2 = '<div class="main_page_body2">' +
+    let body2 = '<div class="main_page_second_sector">' +
                     '<div class="second_sector_title base_pink_font">UpConnect, a sua software house</div>'+
-                    '<div class="second_sector_text base_font_color">' +
+                    '<div class="main_page_text second_sector_text base_font_color">' +
                         'Aqui combinamos expertise técnica com criatividade para oferecer soluções digitais<br>'+
                         'que transformam a maneira como você faz negócios.' +
                     '</div>' +
-                    '<div class="">'+
-                        
+                    '<div class="cards_container">'+
+                        '<div class="second_page_cards second_page_card1">'+
+                            '<div class="title_second_page_card">'+
+                                'Expertise e<br>'+
+                                'Inovação'+
+                            '</div>'+
+                            '<div class="text_second_page_card">'+
+                                'Utilizamos as tecnologias<br>'+
+                                'mais avançadas e práticas<br>'+
+                                'de design inovadoras para<br>'+
+                                'entregar soluções de alta<br>'+
+                                'qualidade.'+
+                            '</div>'+
+                            '<div class="link_2nd_page_card">'+
+                                '<a href="www.google.com">saiba mais</a>'+
+                            '</div>'+
+                        '</div>'+
+                        '<div class="second_page_cards second_page_card2">'+
+                            '<div class="title_second_page_card">'+
+                                'Foco Total<br>'+
+                                'no Cliente'+
+                            '</div>'+
+                            '<div class="text_second_page_card">'+
+                                'Personalizamos nossos<br>'+
+                                'serviços para atender<br>'+
+                                'às suas necessidades<br>'+
+                                'específicas e garantir<br>'+
+                                'sua total satisfação.'+
+                            '</div>'+
+                            '<div class="link_2nd_page_card">'+
+                                '<a href="www.google.com">saiba mais</a>'+
+                            '</div>'+
+                        '</div>'+
+                        '<div class="second_page_cards second_page_card3">'+
+                            '<div class="title_second_page_card">'+
+                                'Resultado<br>'+
+                                'Comprovados'+
+                            '</div>'+
+                            '<div class="text_second_page_card">'+
+                                'Nossos projetos são<br>'+
+                                'desenvolvidos para alcançar<br>'+
+                                'resultados mensuráveis,<br>'+
+                                'aumentando a eficiência e o<br>'+
+                                'retorno sobre o investimento'+
+                            '</div>'+
+                            '<div class="link_2nd_page_card">'+
+                                '<a href="www.google.com">saiba mais</a>'+
+                            '</div>'+
+                        '</div>'+
                     '</div>'+
                 '</div>'
 
-    return body + body2
+    let body3 = '<div class="main_page_third_sector">' +
+                    '<div class="third_sector_title base_pink_font">Nossos Serviços</div>'+
+                    '<div class="main_page_text third_sector_text base_font_color">' +
+                        'Estamos prontos para atender às suas necessidades específicas e<br>'+
+                        'superar suas expectativas' +
+                    '</div>' +
+                    '<div class="third_sector_grids_container">'+
+                        '<div class="third_sector_grid third_sector_grid1">'+
+                            '<div class="third_sector_grid1_info">'+
+                                '<div class="third_sector_grid_title base_pink_font">Desenvolvimento<br>'+
+                                'de Websites</div>'+
+                                '<div class="main_page_text third_sector_grid_text base_font_color">' +
+                                    'Criamos websites personalizados,<br>'+
+                                    'responsivos e otimizados para SEO,<br>'+
+                                    'garantindo que seu negócio se<br>'+
+                                    'destaque e atraia mais visitantes.' +
+                                '</div>' +
+                            '</div>'+
+                            '<div class="third_sector_grid1_img_container">'+
+                                '<img class="third_sector_grid1_img" src="images/cyber2077boy.png" alt="cyber boy">'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>'+
+                    '<div class="third_sector_grids_container">'+
+                        '<div class="third_sector_grid third_sector_grid2">'+
+                            '<div class="third_sector_grid2_info">'+
+                                '<div class="third_sector_grid_title base_pink_font">Desenvolvimento<br>'+
+                                'de E-Commerce</div>'+
+                                '<div class="main_page_text third_sector_grid_text base_font_color">' +
+                                    'Construímos lojas virtuais seguras e<br>'+
+                                    'eficientes, com integração de<br>'+
+                                    'sistemas de pagamento, gestão de<br>'+
+                                    'estoque e funcionalidades de<br>'+
+                                    'marketing para impulsionar suas<br>'+
+                                    'vendas.' +
+                                '</div>' +
+                            '</div>'+
+                            '<div class="third_sector_grid2_img_container">'+
+                                '<img class="third_sector_grid1_img" src="images/cyber2077boy.png" alt="cyber boy">'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>'+
+                    '<div class="third_sector_grids_container">'+
+                        '<div class="third_sector_grid third_sector_grid3">'+
+                            '<div class="third_sector_grid1_info">'+
+                                '<div class="third_sector_grid_title base_pink_font">Desenvolvimento<br>'+
+                                'de Aplicativos</div>'+
+                                '<div class="main_page_text third_sector_grid_text base_font_color">' +
+                                    'Criamos aplicativos móveis inovadores<br>'+
+                                    'para Android e iOS, proporcionando<br>'+
+                                    'experiências incríveis para seus<br>'+
+                                    'usuários.' +
+                                '</div>' +
+                            '</div>'+
+                            '<div class="third_sector_grid1_img_container">'+
+                                '<img class="third_sector_grid1_img" src="images/cyber2077boy.png" alt="cyber boy">'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'
+
+    let body4 = '<div class="main_page_fourth_sector">' +
+                    '<div class="second_sector_title base_pink_font">Outros Serviços</div>'+
+                    '<div class="main_page_text second_sector_text base_font_color">' +
+                        'Se você é uma pequena ou média empresa, uma startup em crescimento, ou uma grande<br>'+
+                        ' corporação buscando transformação digital, estamos aqui para ajudar.' +
+                    '</div>' +
+                    '<div class="cards_container4">'+
+                        '<div class="second_page_cards second_page_card1">'+
+                            '<div class="title_second_page_card">'+
+                                'Marketing Digital<br>'+
+                                'e Social Media'+
+                            '</div>'+
+                            '<div class="text_second_page_card">'+
+                                'Ajudamos a sua empresa a<br>'+
+                                'digitalizar processos,<br>'+
+                                'melhorar a eficiência nas<br>'+
+                                'redes e usar tecnologias<br>'+
+                                'avançadas para <b>impulsionar<br>'+
+                                'o seu crescimento</b>.'+
+                            '</div>'+
+                            '<div class="link_2nd_page_card">'+
+                                '<a href="www.google.com">saiba mais</a>'+
+                            '</div>'+
+                        '</div>'+
+                        '<div class="second_page_cards second_page_card2">'+
+                            '<div class="title_second_page_card">'+
+                                'Design Gráfico e<br>'+
+                                'Identidade Visual'+
+                            '</div>'+
+                            '<div class="text_second_page_card">'+
+                                'Desevolvemos logotipos<br>'+
+                                'identitade visual e<br>'+
+                                'materiais gráficos que<br>'+
+                                'refletem a essência da<br>'+
+                                'sua marca e <b>atraem<br>'+
+                                'seu público-alvo</b>.'+
+                            '</div>'+
+                            '<div class="link_2nd_page_card">'+
+                                '<a href="www.google.com">saiba mais</a>'+
+                            '</div>'+
+                        '</div>'+
+                        '<div class="second_page_cards second_page_card3">'+
+                            '<div class="title_second_page_card">'+
+                                'Hospedagem e<br>'+
+                                'Manutenção'+
+                            '</div>'+
+                            '<div class="text_second_page_card">'+
+                                'Oferecemos serviços de<br>'+
+                                'hospedagem, manutenção e<br>'+
+                                'atualizações periódicas<br>'+
+                                'para garantir que seu<br>'+
+                                'website esteja sempre em<br>'+
+                                '<b>perfeito funcionamento</b>'+
+                            '</div>'+
+                            '<div class="link_2nd_page_card">'+
+                                '<a href="www.google.com">saiba mais</a>'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'
+
+    let footer = '<div class="main_page_footer">'+
+                '<div class="main_page_footer_title">'+
+                    'UpConnect'+
+                '</div>'+
+                '<div class="main_page_footer_last_line">'+
+                    '© 2024 All Rights Reserved'+
+                '</div>'+
+                '<div class="main_page_footer_contact">'+
+                    '<div class="main_page_footer_contact_title">'+
+                        'Nosso contato'+
+                    '</div>'+
+                    '<div class="main_page_footer_contact_number">'+
+                        '+55 11 <b style="font-size: 22px">98921-3124</b>'+
+                    '</div>'+
+                    '<div class="main_page_footer_contact_email">'+
+                        'upconnectweb@gmail.com'+
+                    '</div>'+
+                '</div>'+
+            '</div>'
+
+    return body + body2 + body3 + body4 + footer
 }
